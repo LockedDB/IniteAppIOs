79b57cf8787ea882c43ce2dc8861d622875df959
Daniel <daniel.benet@outlook.es>
2023-06-30 20:52:07 +0200

    Initial Commit

---
+++ IniteAppIOs/IniteAppIOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IniteAppIOsApp.swift
+//  IniteAppIOs
+//
+//  Created by Dani Benet on 30/6/23.
+//
+
+import SwiftUI
+
+@main
+struct IniteAppIOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
